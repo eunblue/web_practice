5a9beeb75c13e5f1a2fa75a620728ba570e713db
eun Ji <tladmswl4869@naver.com>
2022-01-11 23:49:08 +0900

    버튼

--- 플젝 2차.js
+++ 플젝 2차.js
@@ -2,7 +2,7 @@
 const btn=document.querySelector("#button");
 let count=0
 
-btn.addEventListener("click",counter) //파라미터가 2개 첫 번째는 행위, 두 번째는 함수이름
+btn.addEventListener("click",getRadioandResponse) //파라미터가 2개 첫 번째는 행위, 두 번째는 함수이름
 
 function responseText(){
     const message=document.querySelector("#message")
@@ -22,5 +22,27 @@ function counter(){
     message.innerHTML=`현재 카운트 수는 ${count++}입니다.` //빽틱
     if(count>=5){
         message.style.color="pink"
+    }
+}
+
+function getMessageandResponse(){
+    const cin=document.querySelector("#cin")
+    const message=document.querySelector("#message")
+    message.innerHTML=`당신이 입력한건 ${cin.value}군요~!`
+}
+
+function getRadioandResponse(){
+    // const rdo=document.querySelector("#rdo")
+    // const message=document.querySelector("#message")
+    // if(rdo.checked){
+    //     alert("체크 안됑~~")
+    //     rdo.checked=false
+    // }
+    const rdoBtns=document.querySelectorAll("#rdo") //배열로 들어감
+    for(let rdoBtn of rdoBtns){
+        if(rdoBtn.checked&&rdoBtn.value=="bad"){
+            alert("나빠요!")
+            rdoBtn.checked=false
+        }
     }
 }
